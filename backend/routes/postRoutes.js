65986f6f254b0cbfa8d8d7917cc9ac6e97406165
Arlenson Lorran <arlensonlorran@gmail.com>
2026-06-08 22:07:44 -0300

    CRUD completo

---
+++ backend/routes/postRoutes.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const router = express.Router();
+const Post = require("../models/Post");
+
+router.get("/", async (req, res) => {
+    try {
+        const posts = await Post.find();
+        res.status(200).json(posts);
+    } catch (error) {
+        res.status(500).json({ message: "Erro ao buscar posts", details: error.message });
+    }
+})
+
+router.get("/:id", async (req, res) => {
+    try {
+        const idPost = req.params.id;
+        const postFound = await Post.findById(idPost);
+        if (!postFound) {
+            return res.status(404).json({ message: "Post não encontrado" });
+        }
+        res.status(200).json(postFound);
+    } catch (error) {
+        res.status(500).json({ message: "Erro ao buscar post", details: error.message });
+    }
+});
+router.post("/", async (req, res) => {
+    try {
+        const novoPost = new Post({
+            title: req.body.title,
+            content: req.body.content,
+            imageUrl: req.body.imageUrl
+        });
+        const postSalvo = await novoPost.save();
+        res.status(201).json(postSalvo);    
+    } catch (error) {
+        res.status(400).json({ message: "Erro ao criar post", error: error.message });
+    }
+});
+
+router.put("/:id", async (req, res) => {
+    try {
+        const idPost = req.params.id;
+        const newData = req.body;
+        const postUpdated = await Post.findByIdAndUpdate(idPost, newData, { new: true });
+        if (!postUpdated) {
+            return res.status(404).json({ message: "Post não encontrado" });
+        }
+        res.status(200).json(postUpdated);
+    } catch (error) {
+        res.status(500).json({ message: "Erro ao atualizar post", details: error.message });
+    }
+});
+
+router.delete("/:id", async (req, res) => {
+    try {
+        const idPost = req.params.id;
+        postDeleted = await Post.findByIdAndDelete(idPost);
+        if (!postDeleted) {
+            return res.status(404).json({ message: "Post não encontrado para exclusão" });
+        }
+        res.status(200).json({ message: "Post excluído" });
+    
+    } catch (error) {
+        res.status(500).json({ message: "Erro ao excluir post", details: error.message });
+    }
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
+const PORT = process.env.PORT || 5000;
 
 const app = express();
+const postRoutes = require("./routes/postRoutes");
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/posts", postRoutes);
 
 // pegando a connection string do arquivo .env
 const mongoUri = process.env.MONGO_URI;
@@ -16,5 +19,9 @@ const mongoUri = process.env.MONGO_URI;
 mongoose.connect(mongoUri)
     .then(() => { console.log('Connected to MongoDB'); })
     .catch((error) => { console.error('Error connecting to MongoDB:', error); });
+
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
 
     
